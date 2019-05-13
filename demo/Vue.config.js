897f0727ec0f02d13b073c2ac85d1752f5793ae3
wofly <13092945295@163.com>
2019-05-13 09:07:46 +0800

    改

--- demo/Vue.config.js
+++ demo/Vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
     devServer : {
         proxy : {
             "/api":{
-                "target" : "http://localhost",
+                "target" : "http://10.9.25.38:80",
                 "changeOrigin" : true ,
                 "pathRewrite":{
                     "^/api":""
